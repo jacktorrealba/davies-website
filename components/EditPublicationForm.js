b3af8ac2b926367c0b4cae9cc5b3870fcf562b92
Jack Torrealba <jacktorrealba@Jacks-MacBook-Pro.local>
2024-08-15 21:37:39 -0400

    fixed styling

--- components/EditPublicationForm.js
+++ components/EditPublicationForm.js
@@ -3,7 +3,7 @@
 import { useState } from "react"
 import { getApiURL } from "./getApiURL";
 import { useRouter } from "next/navigation";
-import {Center, Card, CardHeader,Heading,CardBody,FormControl,FormLabel, Input, Textarea, Button, Link, CardFooter, Checkbox} from "@chakra-ui/react"
+import {Center, Card, CardHeader,Heading,CardBody,FormControl,FormLabel, Input, Textarea, Button, Link, CardFooter, Checkbox, Box, ChakraProvider} from "@chakra-ui/react"
 import { formatDateDefault } from "@/utils/dateUtils";
 
 export default function EditPublicationForm({id, title, description, datePublished, url, isShown}) {
@@ -37,48 +37,51 @@ export default function EditPublicationForm({id, title, description, datePublish
     }
     return (
         <>
+            <ChakraProvider>
             <Center id="publicationCenter" h="100vh">
-                <Center w="100%" p="2rem 2rem 10rem 2rem">
-                    <Card id="newPublicationCard" w="80%" >
-                        <CardHeader>
-                            <Heading as='h2' size='lg' color="var(--pink)">
-                                Edit Publication
-                            </Heading>
-                        </CardHeader>
-                        <CardBody>
-                            <form onSubmit={handleSubmit}>
-                                <FormControl mb="3">
-                                    <FormLabel>Title</FormLabel>
-                                    <Input onChange={(e) => setNewTitle(e.target.value)} value={newTitle} type='text' variant='filled'></Input>
-                                </FormControl>
-                                <FormControl mb="3">
-                                    <FormLabel>Description</FormLabel>
-                                    <Textarea onChange={(e) => setNewDescription(e.target.value)} value={newDescription} type='text' variant='filled'></Textarea>
-                                </FormControl>
-                                <FormControl mb="3">
-                                    <FormLabel>Date Published</FormLabel>
-                                    <Input onChange={(e) => setNewDatePublished(e.target.value)} value={newDatePublished} type='date' variant='filled'></Input>
-                                </FormControl>
-                                <FormControl mb="3">
-                                    <FormLabel>Link</FormLabel>
-                                    <Input onChange={(e) => setNewURL(e.target.value)} value={newURL} type='text' variant='filled'></Input>
-                                </FormControl>
-                                <FormControl mb="3">
-                                    <Checkbox onChange={(e) => setIsShown(e.target.checked)} isChecked={newIsShown} colorScheme="pink">Enable Publication</Checkbox>
-                                </FormControl>
-                                <Center>
-                                    <Button type='submit' className='pink-button'>Update</Button>
-                                </Center>
-                            </form>
-                        </CardBody>
-                        <CardFooter>
-                            <Center width='100%'>
-                                <Link className="pinkLink-sm" href="/Manage">Cancel</Link>
+                <Card id="newPublicationCard" w="80%" >
+                    <CardHeader>
+                        <Heading as='h2' size='lg' color="var(--pink)">
+                            Edit Publication
+                        </Heading>
+                    </CardHeader>
+                    <CardBody >
+                        <form onSubmit={handleSubmit}>
+                            
+                            <FormControl mb="3">
+                                <FormLabel>Title</FormLabel>
+                                <Input onChange={(e) => setNewTitle(e.target.value)} value={newTitle} type='text' variant='filled'></Input>
+                            </FormControl>
+                            <FormControl mb="3">
+                                <FormLabel>Description</FormLabel>
+                                <Textarea onChange={(e) => setNewDescription(e.target.value)} value={newDescription} type='text' variant='filled'></Textarea>
+                            </FormControl>
+                            <FormControl mb="3">
+                                <FormLabel>Date Published</FormLabel>
+                                <Input onChange={(e) => setNewDatePublished(e.target.value)} value={newDatePublished} type='date' variant='filled'></Input>
+                            </FormControl>
+                            <FormControl mb="3">
+                                <FormLabel>Link</FormLabel>
+                                <Input onChange={(e) => setNewURL(e.target.value)} value={newURL} type='text' variant='filled'></Input>
+                            </FormControl>
+                            <FormControl mb="3">
+                                <Checkbox onChange={(e) => setIsShown(e.target.checked)} isChecked={newIsShown} colorScheme="pink">Enable Publication</Checkbox>
+                            </FormControl>
+                            <Center>
+                                <Button type='submit' className='pink-button'>Update</Button>
                             </Center>
-                        </CardFooter>
-                    </Card>
-                </Center>
-            </Center>
+                            
+                        </form>
+                    </CardBody>
+                    <CardFooter>
+                        <Center width='100%'>
+                            <Link className="pinkLink-sm" href="/Manage">Cancel</Link>
+                        </Center>
+                    </CardFooter>
+                </Card>
+        </Center>
+            </ChakraProvider>
+            
         </>
     )
 }
